ac34bad97d2a6ebef62521a2da2d22b7f42bec8f
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2025-08-21 07:19:08 +0000

    python

---
+++ class2.py
@@ -0,0 +1,12 @@
+# comments, Escape sequence & print statement
+
+print ( "I am good boy \nalso my frd is gud boy")
+
+# \n for th new line
+#  is comment indicator. it does not print 
+#  \"somthing else\" it is escape char
+
+# e.g escape
+
+print("ram is a gud boy \"this boy also gud boy\" acc to ram")
+
